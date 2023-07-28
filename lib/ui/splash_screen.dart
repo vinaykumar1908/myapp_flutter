85b58b28706812a40e1a7cd0540e4b50055d3ede
Vinay Kumar <vinaykumar1908@gmail.com>
2023-07-29 00:10:19 +0530

    splash_screen splash_services ui

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/firebase_services/splash_services.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,8 +9,23 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  SplashServices splashScreen = SplashServices();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    splashScreen.isLogin((context));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text("Hello Bhai"));
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Firebase',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
   }
 }
